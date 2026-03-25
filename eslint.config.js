998c8a31444b1f1fcf7e08d76c75c21c1348a5db
Emre Milijic <em.milijic@gmail.com>
2026-03-25 11:39:27 +0100

    added config for linting

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+export default [
+  {
+    files: ["**/*.js"],
+    rules: {
+      "semi": ["error", "always"],       // zwingt Semikolons
+      "quotes": ["error", "double"]      // zwingt doppelte Anführungszeichen
+    }
+  }
+];
