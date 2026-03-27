e394c6819a6213fcd99c08537a5e8913a94d1414
Remmers, Dean [RTL Deutschland] <dean.remmers@rtl.de>
2026-03-27 11:20:05 +0100

    temporary changes

---
+++ Frontend/data.js
@@ -0,0 +1,17 @@
+const quizData = {
+    "Allgemeinwissen": [
+        { q: "Was ist die Hauptstadt von Frankreich?", a: [{t: "Paris", c: true}, {t: "Marseille", c: false}] },
+        { q: "Wie viele Planeten hat unser Sonnensystem?", a: [{t: "8", c: true}, {t: "9", c: false}] },
+        { q: "Welches Metall ist bei Raumtemperatur flüssig?", a: [{t: "Quecksilber", c: true}, {t: "Blei", c: false}] }
+    ],
+    "Programmierung": [
+        { q: "Wofür steht CSS?", a: [{t: "Cascading Style Sheets", c: true}, {t: "Computer Style System", c: false}] },
+        { q: "Welches Zeichen leitet eine ID in CSS ein?", a: [{t: "#", c: true}, {t: ".", c: false}] },
+        { q: "Welche Sprache wird für Web-Logik genutzt?", a: [{t: "JavaScript", c: true}, {t: "HTML", c: false}] }
+    ],
+    "Gaming": [
+        { q: "Wer ist die Hauptfigur in 'The Legend of Zelda'?", a: [{t: "Link", c: true}, {t: "Zelda", c: false}] },
+        { q: "In welchem Jahr erschien Minecraft?", a: [{t: "2011", c: true}, {t: "2009", c: false}] },
+        { q: "Welches Studio entwickelte 'The Witcher'?", a: [{t: "CD Projekt Red", c: true}, {t: "Ubisoft", c: false}] }
+    ]
+};
